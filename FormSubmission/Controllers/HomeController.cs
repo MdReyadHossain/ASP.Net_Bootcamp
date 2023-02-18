ddd982394ac5f25f4f914eb43db3f2094842aa14
Reyad <69958555+MdReyadHossain@users.noreply.github.com>
2023-02-19 03:21:03 +0600

    database insertion

---
+++ FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FormSubmission.Entity;
+using FormSubmission.Models;
+
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel login)
+        {
+            return View(login);
+        }
+
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Registration(DotNetUser registration)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new ASPDBEntities();
+                db.DotNetUsers.Add(registration);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(registration);
+        }
+    }
+}
---
+++ FormSubmission/Models/RegistrationModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmission.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "Name should not be empty!")]
+        [StringLength(10, ErrorMessage = "Name should not be exceed 10 characters!")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Please select your gender!")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please select your Profession!")]
+        public string Profession { get; set; }
+
+        [Required(ErrorMessage = "Please select your hobbies!")]
+        public string[] Hobbies { get; set; }
+
+        [Required(ErrorMessage = "Please provide your date of birth!")]
+        public DateTime DOB { get; set; }
+
+        [Required(ErrorMessage = "Password should not be empty!")]
+        [MinLength(6, ErrorMessage = "Password should be minimum 6 characters")]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "Retype your password again!")]
+        [Compare("Password", ErrorMessage = "Password does not matched!")]
+        public string RePassword { get; set; }
+    }
+}
