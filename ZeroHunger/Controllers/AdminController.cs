753e48cf9c2ece160893fb310976b04f80e840f3
Reyad <69958555+MdReyadHossain@users.noreply.github.com>
2023-03-21 06:15:35 +0600

    request sent done

---
+++ ZeroHunger/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.Auth;
+
+namespace ZeroHunger.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        [AdminAccess]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
---
+++ ZeroHunger/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.Auth;
+
+namespace ZeroHunger.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [EmployeeAccess]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
